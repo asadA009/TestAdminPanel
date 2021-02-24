34b8c4d7db4cf1884b30e83ec6fef42cea4a33ca
asadA009 <asadali4345425@gmail.com>
2021-02-24 14:08:39 +0500

    third

--- django_adminlte3/settings.py
+++ django_adminlte3/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # STATICFILES_DIRS = [
 #      os.path.join(BASE_DIR+'/bower_components'),
 # ]
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_SORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
 django_heroku.settings(locals())
